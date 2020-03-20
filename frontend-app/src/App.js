e6a97f42bd04fdaa82f6607aa420b56d335da1a5
Tony Antelman <AntelmanAnthonyJ@JohnDeere.com>
2020-03-20 17:04:42 -0500

    initial layout setup in app

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <body class="App">
+        <header>Header</header>
+        <div class="App-body">
+          <main class="App-content">Main Content</main>
+          <nav class="App-nav">
+            <h1>Navigation</h1>
+              <ul>
+                <li>Shows</li>
+                <li>Event Calendar</li>
+                <li>Resources</li>
+                <li>Products</li>
+                <li>Equipment</li>
+                <li>Customers</li>
+              </ul>
+          </nav>
+          <aside class="App-sidebar">Sidebar</aside>
+        </div>
+        <footer>Footer</footer>
+      </body>
+    );
+  }
+  
 }
 
 export default App;
